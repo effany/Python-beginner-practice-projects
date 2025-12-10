756fd74f98749c6cd6dcde092a9b969271905a4c
effany <effany28119@gmail.com>
2025-12-10 10:54:45 +0100

    day 40

---
+++ Intermediate-projects/Flight-club/data_manager.py
@@ -0,0 +1,50 @@
+import os
+import requests
+from requests.auth import HTTPBasicAuth
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/69f7db387d564a87133246560a716a23/flightDeals/prices"
+SHEETY_USERS_ENDPOINT = "https://api.sheety.co/69f7db387d564a87133246560a716a23/flightDeals/users"
+
+class DataManager:
+
+    def __init__(self):
+        self._user = os.environ["SHEETY_USRERNAME"]
+        self._password = os.environ["SHEETY_PASSWORD"]
+        self._authorization = HTTPBasicAuth(self._user, self._password)
+        self.destination_data = {}
+        self.user_emails = []
+
+    def get_destination_data(self):
+        # Use the Sheety API to GET all the data in that sheet and print it out.
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT, auth=self._authorization)
+        data = response.json()
+        self.destination_data = data["prices"]
+        # Try importing pretty print and printing the data out again using pprint() to see it formatted.
+        # pprint(data)
+        return self.destination_data
+
+    # In the DataManager Class make a PUT request and use the row id from sheet_data
+    # to update the Google Sheet with the IATA codes. (Do this using code).
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data,
+                auth=self._authorization
+            )
+            print(response.text)
+
+    
+    def get_user_emails(self):
+        response = requests.get(url=SHEETY_USERS_ENDPOINT, auth=self._authorization)
+        data = response.json()
+        return [user["email"] for user in data["users"]]
---
+++ Intermediate-projects/Flight-club/notification_manager.py
@@ -0,0 +1,73 @@
+import os
+from dotenv import load_dotenv
+from twilio.rest import Client
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+load_dotenv()
+# Using a .env file to retrieve the phone numbers and tokens.
+
+class NotificationManager:
+
+    def __init__(self):
+        self.client = Client(os.environ['TWILIO_SID'], os.environ["TWILIO_AUTH_TOKEN"])
+
+    def send_sms(self, message_body):
+        """
+        Sends an SMS message through the Twilio API.
+        This function takes a message body as input and uses the Twilio API to send an SMS from
+        a predefined virtual number (provided by Twilio) to your own "verified" number.
+        It logs the unique SID (Session ID) of the message, which can be used to
+        verify that the message was sent successfully.
+
+        Parameters:
+        message_body (str): The text content of the SMS message to be sent.
+
+        Returns:
+        None
+
+        Notes:
+        - Ensure that `TWILIO_VIRTUAL_NUMBER` and `TWILIO_VERIFIED_NUMBER` are correctly set up in
+        your environment (.env file) and correspond with numbers registered and verified in your
+        Twilio account.
+        - The Twilio client (`self.client`) should be initialized and authenticated with your
+        Twilio account credentials prior to using this function when the Notification Manager gets
+        initialized.
+        """
+        message = self.client.messages.create(
+            from_=os.environ["TWILIO_VIRTUAL_NUMBER"],
+            body=message_body,
+            to=os.environ["TWILIO_VIRTUAL_NUMBER"]
+        )
+        # Prints if successfully sent.
+        print(message.sid)
+
+    # Is SMS not working for you or prefer whatsapp? Connect to the WhatsApp Sandbox!
+    # https://console.twilio.com/us1/develop/sms/try-it-out/whatsapp-learn
+    def send_whatsapp(self, message_body):
+        message = self.client.messages.create(
+            from_=f'whatsapp:{os.environ["TWILIO_WHATSAPP_NUMBER"]}',
+            body=message_body,
+            to=f'whatsapp:{os.environ["TWILIO_VERIFIED_NUMBER"]}'
+        )
+        print(message.sid)
+
+    
+    def send_deals_emails(self, message_body, emails):
+        my_email = os.environ.get("GMAIL_USERNAME")
+        password = os.environ.get("GMAIL_APP_PASSWORD")   
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            for email in emails:
+                # Create a proper MIME message with UTF-8 encoding
+                msg = MIMEMultipart()
+                msg['From'] = my_email
+                msg['To'] = email
+                msg['Subject'] = "Flight Deal Alert!"
+                
+                # Attach the message body with UTF-8 encoding
+                msg.attach(MIMEText(message_body, 'plain', 'utf-8'))
+                
+                connection.sendmail(from_addr=my_email, to_addrs=email, msg=msg.as_string())
